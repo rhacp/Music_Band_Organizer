79297b3fcd1fd743c1ff8db3733a9a856d1bcdec
Andrei <mirceanghel98@gmail.com>
2024-01-08 00:43:56 +0200

    Refactoring the entity

--- src/main/java/com/anghel/music_band_organizer/models/entities/User.java
+++ src/main/java/com/anghel/music_band_organizer/models/entities/User.java
@@ -3,6 +3,10 @@ package com.anghel.music_band_organizer.models.entities;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDate;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @Data
 @Entity
 @Table(name =  "users")
@@ -12,5 +16,23 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "birthday")
+    private LocalDate birthday;
+
+    @Column(name = "age")
+    private Integer age;
+
+    @ElementCollection
+    @CollectionTable(name = "user_past_experience",
+            joinColumns = @JoinColumn(name = "user_id")
+    )
+    @MapKeyColumn(name = "user_name")
+    @Column(name = "past_experience")
+    private Map<String, String> pastExperience = new LinkedHashMap<>();
 }
