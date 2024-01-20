7d510edb87c6d0d459ef317e25a0c9894f604a68
Andrei <mirceanghel98@gmail.com>
2024-01-20 06:35:59 +0200

    refactor OpenAIImpl and OpenAI

--- src/main/java/com/anghel/music_band_organizer/services/open_ai/OpenAI.java
+++ src/main/java/com/anghel/music_band_organizer/services/open_ai/OpenAI.java
@@ -1,6 +1,14 @@
 package com.anghel.music_band_organizer.services.open_ai;
 
+import java.io.BufferedReader;
+
 public interface OpenAI {
 
     String chatGPT(String prompt);
+
+    StringBuilder getJSONResponseFromBuffer(BufferedReader br);
+
+    BufferedReader getBufferedReader(String prompt);
+
+    String extractMessageFromJSONResponse(String response);
 }
