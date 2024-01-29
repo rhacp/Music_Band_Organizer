545f62dad3a19e77f1acbe4f34538e8b52fbddd4
Andrei <mirceanghel98@gmail.com>
2024-01-29 03:16:38 +0200

    Added doccumentation for user services

--- src/main/java/com/anghel/music_band_organizer/services/user/UserService.java
+++ src/main/java/com/anghel/music_band_organizer/services/user/UserService.java
@@ -12,24 +12,51 @@ import java.util.List;
 public interface UserService {
 
     /**
-     * Creates a User based on DTO.
+     * Creates a user based on the given userDTO.
      * @param userDTO
      * @return UserDTO of the saved user
      */
     UserDTO createUser(UserDTO userDTO);
 
     /**
-     * Returns all users from DB.
-     * @return UserDTO List
+     * Returns the list of all existing users.
+     * @return List of UserDTO
      */
     List<GetAllUsersDTO> getAllUsers();
 
+    /**
+     * Return a user based on id.
+     * @param userId user id to search for
+     * @return UserDTO returned user DTO
+     */
     UserDTO getUserById(Long userId);
 
+    /**
+     * Delete user by the given id.
+     * @param userId user id to delete
+     * @return String message
+     */
     String deleteUserById(Long userId);
 
+    /**
+     * Update user by the given id and returns it.
+     * @param userId
+     * @param userDTO
+     * @return UserDTO
+     */
     UserDTO updateUserById(Long userId, UserDTO userDTO);
 
+    /**
+     * Returns a list of filtered users on different criteria.
+     * @param userId user id for filtering
+     * @param firstName user first name for filtering
+     * @param lastName user last name for filtering
+     * @param userEmail user email for filtering
+     * @param userBirthday user birthday for filtering
+     * @param pastExperience user past experience map key for filtering
+     * @param userStageName user id for filtering
+     * @return List of UserDTO
+     */
     List<UserDTO> getFilteredUsers(Long userId,
                                    String firstName,
                                    String lastName,
